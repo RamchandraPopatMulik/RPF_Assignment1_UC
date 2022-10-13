a83e04760d095d9bd8d422451ce58a7ab4abe7d5
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-13 17:07:29 +0530

    Added uc file

---
+++ UC-1Employees/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace UC_1Employees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int IS_FULLTIME = 1;
+            Random random = new Random();
+            int val = random.Next(0,2);
+            if (val == IS_FULLTIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
